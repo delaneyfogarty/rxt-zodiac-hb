49f29f4084bc207aa60acaf520d5ecb2a94e052a
Delaney Fogarty <delaney.fk@gmail.com>
2022-04-25 16:34:11 -0700

    I did it!

--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -1,5 +1,16 @@
 import './ZodiacCard.css';
 
-export default function ZodiacCard() {
-  {/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */}
+
+
+export default function ZodiacCard({ zodiac }) {
+  return (
+    <div className='zodiac-card'>
+      <p>{zodiac.name}</p>
+      <p>{zodiac.dates}</p>
+      <img src={`./images/${zodiac.name}.png`}/>
+    </div>
+
+  );
 }
+
+{/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */}
